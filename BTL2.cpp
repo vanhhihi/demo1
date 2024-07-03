858f9117b03d297cc6879fb560f527bbc39c39f3
vanhhihi <leva2k5@gmail.com>
2024-07-03 09:11:52 +0700

    new

--- BTL2.cpp
+++ BTL2.cpp
@@ -20,16 +20,16 @@ public:
     }
     void MENU(){
        system("cls");
-        printf("================QUAN LY HE THONG================");
-        printf("\n1. Quan ly do uong ");
-        printf("\n2. Mua hang");
-        printf("\n3. Quan ly nhan vien ");
-        printf("\n4. Luu thong tin do uong");
-        printf("\n5. Luu thong tin nhan vien");
-        printf("\n6. Thoat chuong trinh");
-        printf("\n=================================================");
-        printf("\n chon muc ");
-        scanf("%d",&k);
+        cout<<"================QUAN LY HE THONG================";
+        cout<<"\n1. Quan ly do uong ";
+        cout<<"\n2. Mua hang";
+        cout<<"\n3. Quan ly nhan vien ";
+        cout<<"\n4. Luu thong tin do uong";
+        cout<<"\n5. Luu thong tin nhan vien";
+        cout<<"\n6. Thoat chuong trinh";
+        cout<<"\n=================================================";
+        cout<<"\n chon muc ";
+        cin>>k;
         switch (k){
             case 1: 
             QLDO();         
@@ -59,14 +59,14 @@ public:
     void menuchung::QLDO(){
         int k;
         system("cls");
-        printf("================QUAN LY DO UONG================");
-        printf("\n1. Them do uong");
-        printf("\n2. Xoa do uong ");
-        printf("\n3. thong tin cua tat ca do uong");    
-        printf("\n4. Quay lai");
-        printf("\n==============================================");
-        printf("\nchon muc : ");
-        scanf("%d",&k);
+        cout<<"================QUAN LY DO UONG================";
+        cout<<"\n1. Them do uong";
+        cout<<"\n2. Xoa do uong ";
+        cout<<"\n3. thong tin cua tat ca do uong";    
+        cout<<"\n4. Quay lai";
+        cout<<"\n==============================================";
+        cout<<"\nchon muc : ";
+        cin>>k;
         fflush(stdin);
         switch(k){
             case 1:
@@ -82,7 +82,7 @@ public:
                 MENU();
                 break;
             default : {
-                printf("co ve ban chon sai roi");
+                cout<<("co ve ban chon sai roi");
                 sleep(1);
                 QLDO();
                 break;
@@ -93,19 +93,19 @@ public:
         int k;
         system("cls");
         int plus;
-        printf("muon them bao nhieu do uong vao : ");
-        scanf("%d",&plus);
+        cout<<"muon them bao nhieu do uong vao : ";
+        cin>>plus;
         fflush(stdin);
         drink.resize(namedrink::n+plus);
         for(int i=namedrink::n;i<namedrink::n+plus;i++)
             nhapnamedrink(&drink[i]);
         namedrink::n+=plus;
-        printf("\n============================");
-        printf("\n1. tiep tuc them do uong ");
-        printf("\n2. quay lai");
-        printf("\n============================");
-        printf("\n");
-        scanf("%d",&k);
+        cout<<"\n============================";
+        cout<<"\n1. tiep tuc them do uong ";
+        cout<<"\n2. quay lai";
+        cout<<"\n============================";
+        cout<<"\n";
+        cin>>k;
         switch(k){
             case(1):
                 TDO();
@@ -119,26 +119,26 @@ public:
         int k,a;
         char c;
         system("cls");
-        printf("============================");
-        printf("\n");
+        cout<<"============================";
+        cout<<"\n";
         for(int i=0;i<namedrink::n;i++){ 
-            printf("%d .",i+1);
+            cout<<("%d .",i+1);
             xuatdrink(drink[i]);
-            printf("\n");
+            cout<<"\n";
         }
-        printf("\n============================");
-        printf("\nban muon xoa do uong nao : ");
-        scanf("%d",&k);
+        cout<<"\n============================";
+        cout<<"\nban muon xoa do uong nao : ";
+        cin>>k;
      //   themrubbish(rubbish[rubbish::number],drink[k-1]);
         drink.erase(drink.begin()+(k-1));
         namedrink::n--;
         system("cls");
-        printf("============================");
-      //  printf("\nban da xoa do uong : ");
+        cout<<"============================";
+      //  cout<<("\nban da xoa do uong : ");
      //   xuatrubbish(rubbish[rubbish::number-1]);
-        printf("\n1. Tiep tuc xoa ");
-        printf("\n2. Quay lai ");
-        scanf("%d",&a);
+        cout<<"\n1. Tiep tuc xoa ";
+        cout<<"\n2. Quay lai ";
+        cin>>a;
         switch(a){
             case 1:
                 XDO();
@@ -151,19 +151,19 @@ public:
     void menuchung::TTDO(){
         int k;
         system("cls");
-        printf("============================");
+        cout<<"============================";
         for(int i=0 ; i<namedrink::n;i++){ 
-        printf("\n");
-        printf("%d. ",i+1);   
+        cout<<"\n";
+        cout<<i+1;   
         xuatdrink(drink[i]);
-        printf("\n");
-        printf("============================");
+        cout<<"\n";
+        cout<<"============================";
         }
-        //printf("\n1. chon do uong");
-        printf("\n1. them do uong");
-        printf("\n2. xoa do uong");
-        printf("\n3. quay lai");
-        scanf("%d",&k);
+        //cout<<("\n1. chon do uong");
+        cout<<"\n1. them do uong";
+        cout<<"\n2. xoa do uong";
+        cout<<"\n3. quay lai";
+        cin>>k;
         switch(k){
             case 1:
                 TDO();
@@ -180,17 +180,17 @@ public:
 void menuchung::BUY(){
     int pick;
     system("cls");
-    printf("------------------------------------------------------------\n");
-    printf("| %3s | %25s \t|  %10s   |\n","STT","TEN DO UONG"," GIA DO UONG ");
+    cout<<"------------------------------------------------------------\n";
+    cout<<"STT","TEN DO UONG"," GIA DO UONG ";
     for(int i= 0 ; i< namedrink::n;i++){
-        printf("------------------------------------------------------------\n");
-        printf("| %2d  ",i+1);
+        cout<<"------------------------------------------------------------\n";
+        cout<<i+1;
         bangnamedrink(drink[i]);
     }
-    printf("------------------------------------------------------------\n");
-    printf("1. tao bill");
-    printf("\n2. quay lai");
-    printf("\nchon muc :  ");
+    cout<<"------------------------------------------------------------\n";
+    cout<<"1. tao bill";
+    cout<<"\n2. quay lai";
+    cout<<"\nchon muc :  ";
     cin>>pick;
     switch(pick){
         case 1 : 
@@ -211,27 +211,27 @@ void menuchung::taobill(){
         int thoat =1 ;
         xoadong(4);
         bill.resize(Bill::n+1);
-       // printf("ban chon do uong nao : \n");
-       // printf("chon 0 neu ban ko muon mua :\n");
-       // scanf("%d",&pick);
+       // cout<<("ban chon do uong nao : \n");
+       // cout<<("chon 0 neu ban ko muon mua :\n");
+       // cin>>("%d",&pick);
         while(thoat){
-            printf("chon 0 neu ban ko muon mua :\n");
-            printf("ban chon do uong nao : \n");
-            scanf("%d",&pick);
+            cout<<"chon 0 neu ban ko muon mua :\n";
+            cout<<"ban chon do uong nao : \n";
+            cin>>pick;
             if(pick == 0 ) break;
             xoadong(4);
             bill[Bill::n].billdrink.resize(bill[Bill::n].number+1);
             strcpy( bill[Bill::n].billdrink[bill[Bill::n].number].name , drink[pick-1].name);
             bill[Bill::n].billdrink[bill[Bill::n].number].cost = drink[pick-1].cost;
-            printf("muon mua bao nhieu %s : ", drink[pick-1].name);
+            cout<<("muon mua bao nhieu %s : ", drink[pick-1].name);
             cin>>bill[Bill::n].billdrink[bill[Bill::n].number].soluong;
             bill[Bill::n].tong += drink[pick-1].cost * bill[Bill::n].billdrink[bill[Bill::n].number].soluong;
             dem =1;
             bill[Bill::n].number++;
             xoadong(2);
-            printf(" mua do uong tiep ko ");
-            printf("\n 1 : co ");
-            printf("\n 0 : ko ");
+            cout<<" mua do uong tiep ko ";
+            cout<<"\n 1 : co ";
+            cout<<"\n 0 : ko ";
             cin>>thoat;
             xoadong(4);
         }
@@ -258,15 +258,15 @@ void menuchung::taobill(){
 }
     void menuchung::QLNV(){
         system("cls");
-        printf("==============QUAN LY NHAN VIEN==============");
-        printf("\n1. Them nhan vien.");
-        printf("\n2. Danh sach nhan vien.");
-        printf("\n3. Tim kiem nhan vien theo keyword.");
-        printf("\n4. Luu du lieu nhan vien.");
-        printf("\n5. Quay lai.");
-        printf("\n=============================================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+        cout<<"==============QUAN LY NHAN VIEN==============";
+        cout<<"\n1. Them nhan vien.";
+        cout<<"\n2. Danh sach nhan vien.";
+        cout<<"\n3. Tim kiem nhan vien theo keyword.";
+        cout<<"\n4. Luu du lieu nhan vien.";
+        cout<<"\n5. Quay lai.";
+        cout<<"\n=============================================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         fflush(stdin);
         switch (k){
             case(1):
@@ -289,12 +289,12 @@ void menuchung::taobill(){
     void menuchung::TNV(){ 
         system("cls");
         Nhapdanhsachnhanvien(ds,t,n,tongluong);
-        printf("\n============================");
-        printf("\n1. Tiep tuc them nhan vien. ");
-        printf("\n2. Quay lai.");
-        printf("\n============================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+        cout<<"\n============================";
+        cout<<"\n1. Tiep tuc them nhan vien. ";
+        cout<<"\n2. Quay lai.";
+        cout<<"\n============================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         switch(k){
             case(1):
                 TNV();
@@ -306,18 +306,19 @@ void menuchung::taobill(){
     } 
 
     void menuchung::XNV(){
-    	printf("Nhap ma cua nhan vien can xoa: ");scanf("%d",&k);
+    	cout<<"Nhap ma cua nhan vien can xoa: ";
+        cin>>k;
         system("cls");
     	XoaNhanVienTheoMa(ds,n,k,tongluong);
-    	printf("\n===================================");
-    	printf("\n------THAO TAC TREN DANH SACH------");
-    	printf("\n1. Xoa nhan vien.");
-    	printf("\n2. Sap xep danh sach theo thu tu alpha B.");
-    	printf("\n3. Sua thong tin nhan vien.");
-        printf("\n4. Quay lai.");
-        printf("\n===================================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+    	cout<<"\n===================================";
+    	cout<<"\n------THAO TAC TREN DANH SACH------";
+    	cout<<"\n1. Xoa nhan vien.";
+    	cout<<"\n2. Sap xep danh sach theo thu tu alpha B.";
+    	cout<<"\n3. Sua thong tin nhan vien.";
+        cout<<"\n4. Quay lai.";
+        cout<<"\n===================================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         switch(k){
             case(1):
                 XNV();
@@ -336,16 +337,16 @@ void menuchung::taobill(){
     void menuchung::TKNV(){
     	char ten[25];
     	system("cls");
-    	printf("Nhap keyword trong ten nhan vien can tim: ");fflush(stdin);
+    	cout<<"Nhap keyword trong ten nhan vien can tim: ";fflush(stdin);
     	fgets(ten,sizeof(ten),stdin);
     	xoaXuongDong(ten);
     	timNhanVienTheoTen(ds,n,ten);
-    	printf("\n==========================");
-    	printf("\n1. Tiep tuc tim kiem.");
-        printf("\n2. Quay lai.");
-        printf("\n==========================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+    	cout<<"\n==========================";
+    	cout<<"\n1. Tiep tuc tim kiem.";
+        cout<<"\n2. Quay lai.";
+        cout<<"\n==========================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         switch(k){
             case(1):
                 TKNV();
@@ -359,15 +360,15 @@ void menuchung::taobill(){
     void menuchung::DSNV(){
     	system("cls");
     	XuatDanhSachNhanVien(ds,n,tongluong);
-    	printf("\n===================================");
-    	printf("\n------THAO TAC TREN DANH SACH------");
-    	printf("\n1. Xoa nhan vien.");
-    	printf("\n2. Sap xep danh sach theo thu tu alpha B. ");
-    	printf("\n3. Sua thong tin nhan vien.");
-        printf("\n4. Quay lai.");
-        printf("\n===================================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+    	cout<<"\n===================================";
+    	cout<<"\n------THAO TAC TREN DANH SACH------";
+    	cout<<"\n1. Xoa nhan vien.";
+    	cout<<"\n2. Sap xep danh sach theo thu tu alpha B. ";
+    	cout<<"\n3. Sua thong tin nhan vien.";
+        cout<<"\n4. Quay lai.";
+        cout<<"\n===================================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         switch(k){
             case(1):
                 XNV();
@@ -387,14 +388,14 @@ void menuchung::taobill(){
     void menuchung::SXDSNV(){
     	system("cls");
     	sapXepDanhSachNhanVienTheoTen(ds,n);
-    	printf("\n===================================");
-    	printf("\n------THAO TAC TREN DANH SACH------");
-    	printf("\n1. Xoa nhan vien.");
-    	printf("\n2. Sua thong tin nhan vien.");
-        printf("\n3. Quay lai.");
-        printf("\n===================================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+    	cout<<"\n===================================";
+    	cout<<"\n------THAO TAC TREN DANH SACH------";
+    	cout<<"\n1. Xoa nhan vien.";
+    	cout<<"\n2. Sua thong tin nhan vien.";
+        cout<<"\n3. Quay lai.";
+        cout<<"\n===================================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         switch(k){
             case(1):
                 XNV();
@@ -408,19 +409,20 @@ void menuchung::taobill(){
         }
     }
     void menuchung::STTNV(){
-    	printf("STT cua nhan vien ban muon sua: ");scanf("%d",&k);
+    	cout<<"STT cua nhan vien ban muon sua: ";
+        cin>>k;
     	SuaNhanVien(ds,n,k,tongluong);
     	system("cls");
     	XuatDanhSachNhanVien(ds,n,tongluong);
-    	printf("\n===================================");
-    	printf("\n------THAO TAC TREN DANH SACH------");
-    	printf("\n1. Xoa nhan vien.");
-    	printf("\n2. Sap xep danh sach theo thu tu alpha B. ");
-    	printf("\n3. Sua thong tin nhan vien.");
-        printf("\n4. Quay lai.");
-        printf("\n===================================");
-        printf("\nChon muc: ");
-        scanf("%d",&k);
+    	cout<<"\n===================================";
+    	cout<<"\n------THAO TAC TREN DANH SACH------";
+    	cout<<"\n1. Xoa nhan vien.";
+    	cout<<"\n2. Sap xep danh sach theo thu tu alpha B. ";
+    	cout<<"\n3. Sua thong tin nhan vien.";
+        cout<<"\n4. Quay lai.";
+        cout<<"\n===================================";
+        cout<<"\nChon muc: ";
+        cin>>k;
         switch(k){
             case(1):
                 XNV();
@@ -444,7 +446,7 @@ void menuchung:: ghifile(){
     FILE*f;
     f=fopen("vanhcpp.bin","wb");
     if(f==NULL){
-        printf("\nloi mo file");
+        cout<<"\nloi mo file";
         return;
     }
     fwrite(&namedrink::n,sizeof(int),1,f);
@@ -455,7 +457,7 @@ void menuchung:: docfile(){
     FILE*f;
     f=fopen("vanhcpp.bin","rb");
     if(f==NULL){
-        printf("\nloi mo file");
+        cout<<("\nloi mo file");
         return;
     }
     fread(&namedrink::n,sizeof(int),1,f);
@@ -474,11 +476,11 @@ int main(){
     string c, d;
     string a = "admin";
     string b = "admin";
-    printf("|------------------------------------------|\n");
-    printf("| TAI KHOAN |                              |\n");
-    printf("|------------------------------------------|\n");
-    printf("| MAT KHAU  |                              |\n");
-    printf("|------------------------------------------|");
+    cout<<"|------------------------------------------|\n";
+    cout<<"| TAI KHOAN |                              |\n";
+    cout<<"|------------------------------------------|\n";
+    cout<<"| MAT KHAU  |                              |\n";
+    cout<<"|------------------------------------------|";
     cout<<"\033[2;14H";
     cin>>c;
     cout<<"\033[4;14H";
@@ -489,7 +491,7 @@ int main(){
 
     if(c == a && d == a){
         sleep(2);
-        printf("OK");
+        cout<<"OK";
         sleep(1);
         menuchung m1;
         DocFile(ds,n,tongluong);
@@ -498,11 +500,13 @@ int main(){
         break;
     }
     
-    if(login == 3){ printf("tai khoan mat khau sai roi");
+    else{ cout<<("tai khoan mat khau sai roi");
     sleep(1);
-        return 0;
     }
-    login++;
+    ++login;
+    if(login ==3 ) {
+        return 0 ;
+    }
     sleep(2);
     }
    
